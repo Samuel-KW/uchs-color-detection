ba6bfbada4f69510f7450c645883e9f8f3017f2e
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2021-12-15 00:39:06 -0800

    Update main.java

--- main.java
+++ main.java
@@ -6,7 +6,7 @@ import android.graphics.BitmapFactory;
 final BitmapFactory.Options options = new BitmapFactory.Options();
 options.inJustDecodeBounds = true;
 
-BitmapFactory.decodeResource(getResources(), R.drawable.test, options);
+BitmapFactory.decodeResource(getResources(), R.drawable.image, options);
 
 int imageHeight = options.outHeight;
 int imageWidth = options.outWidth;
@@ -19,30 +19,23 @@ int[] averages = new int[segments];
 options.inSampleSize = 2;
 options.inJustDecodeBounds = false;
 
-Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.test, options);
+Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.drawable.image, options);
 
-System.out.println("seg length: " + segment_length);
-System.out.println("array length: " + averages.length);
-System.out.println("image width: " + imageWidth);
 
-for (int x = 0; x < imageWidth; x++)
-{
-    for (int y = 0; y < imageHeight; y++)
-    {
-        int pixel = (bmp.getPixel(x, y) >> 8) & 0xff;
+for (int x = 0; x < imageWidth; x++) {
+    for (int y = 0; y < imageHeight; y++) {
+        int pixel = bmp.getPixel(x, y);
+
+        int r = pixel & 0xff;
+        int g = (pixel >> 8) & 0xff;
+        int b = (pixel >> 16) & 0xff;
+
         int index = x / segment_length;
 
-        if (index < segments) averages[index] += pixel;
+        if (g >= r && g >= b && index < segments) averages[index] += 1;
     }
 }
 
-int divisor = segment_length * imageHeight;
-
-for(int i = 0; i < segments; i++)
-{
-    averages[i] = averages[i] / divisor;
-}
-
-System.out.println(averages[2]);
-System.out.println(averages[1]);
 System.out.println(averages[0]);
+System.out.println(averages[1]);
+System.out.println(averages[2]);
